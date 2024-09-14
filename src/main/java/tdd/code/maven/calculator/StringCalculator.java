f4d2a6988aea2c4e4ba7457c07446752c6cb40f7
Hanumant Lad <hanumantlad100@gmail.com>
2024-09-14 13:55:50 +0530

    test: Add test case for empty string returning 0 code: Implement add method to return 0 for empty string

---
+++ src/main/java/tdd/code/maven/calculator/StringCalculator.java
@@ -0,0 +1,8 @@
+package tdd.code.maven.calculator;
+
+public class StringCalculator {
+
+	public int add(String numbers) {
+        return 0;
+    }
+}
--- src/test/java/tdd/code/maven/AppTest.java
+++ src/test/java/tdd/code/maven/AppTest.java
@@ -1,7 +1,11 @@
 package tdd.code.maven;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import tdd.code.maven.calculator.StringCalculator;
 
 /**
  * Unit test for simple App.
@@ -14,5 +18,14 @@ public class AppTest {
     @Test
     public void testApp() {
         assertTrue(true, "This test will always pass");
+    }
+    
+    /**
+     * Test an empty string should return 0
+     */
+    @Test
+    public void testEmptyString() {
+        StringCalculator calculator = new StringCalculator();
+        assertEquals(0, calculator.add(""));
     }
 }
