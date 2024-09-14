4383f015f7f9ef8637ed80e03292a09d123d6111
Hanumant Lad <hanumantlad100@gmail.com>
2024-09-14 13:42:16 +0530

    Initialize project with Maven and JUnit 5 setup

---
+++ src/test/java/tdd/code/maven/AppTest.java
@@ -0,0 +1,18 @@
+package tdd.code.maven;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Simple test to assert that true is indeed true.
+     */
+    @Test
+    public void testApp() {
+        assertTrue(true, "This test will always pass");
+    }
+}
