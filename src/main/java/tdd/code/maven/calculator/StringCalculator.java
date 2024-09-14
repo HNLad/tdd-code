d26a0e5bff7d6ead9790d2f963071c28136c0bed
Hanumant Lad <hanumantlad100@gmail.com>
2024-09-14 15:05:24 +0530

    refactored the code and structure

--- src/main/java/tdd/code/maven/calculator/StringCalculator.java
+++ src/main/java/tdd/code/maven/calculator/StringCalculator.java
@@ -10,154 +10,66 @@ public class StringCalculator {
 			return 0;
 		}
 
-		String delimiter = ",";
+		// Extract delimiter from numbers string
+		String delimiter = getDelimiter(numbers);
+
+		// Get Proper Format Number String from the custom delimiter
+		String properNumbersWithDelimiter = properFormatInput(numbers, delimiter);
+
+		// Split numbers by delimiter and calculate sum
+		return calculateSum(properNumbersWithDelimiter, delimiter);
+	}
+
+	private String getDelimiter(String numbers) {
 		if (numbers.startsWith("//")) {
 			int delimiterIndex = numbers.indexOf("\n");
-			delimiter = numbers.substring(2, delimiterIndex);
-			numbers = numbers.substring(delimiterIndex + 1);
+			return numbers.substring(2, delimiterIndex);
 		}
-		
-		// replace \n by delimiter
-		numbers = numbers.replace("\n", delimiter);
-		
-		// split string
+		// Default delimiter
+		return ",";
+	}
+
+	private String properFormatInput(String numbers, String delimiter) {
+		if (numbers.startsWith("//")) {
+			int delimiterIndex = numbers.indexOf("\n");
+			return numbers.substring(delimiterIndex + 1).replace("\n", delimiter);
+		}
+		return numbers.replace("\n", delimiter);
+	}
+
+	private int calculateSum(String numbers, String delimiter) {
+
 		String[] numArray = numbers.split(delimiter);
+		List<Integer> negativeNumList = new ArrayList<>();
+
 		int sum = 0;
 
-		// handling any negative number in the number string
-		List<Integer> negativeNumbers = new ArrayList<>();
 		for (String num : numArray) {
 			int value = Integer.parseInt(num);
 			if (value < 0) {
-				negativeNumbers.add(value);
+				negativeNumList.add(value);
 			} else {
 				sum += value;
 			}
 		}
 
-		// if negative found throw error
-		if (!negativeNumbers.isEmpty()) {
-
-			// handled multiple negative numbers
-			StringBuilder exceptionMessage = new StringBuilder("Negative numbers not allowed: ");
-			for (int i = 0; i < negativeNumbers.size(); i++) {
-				exceptionMessage.append("[").append(negativeNumbers.get(i)).append("]");
-				if (i < negativeNumbers.size() - 1) {
-					exceptionMessage.append(",");
-				}
-			}
-			throw new RuntimeException(exceptionMessage.toString());
+		// Handle the calculation and exception for negative numbers
+		if (!negativeNumList.isEmpty()) {
+			throw new RuntimeException(formatExceptionMessage(negativeNumList));
 		}
+
 		return sum;
 	}
-	
 
-//	public int add(String numbers) {
-//        return 0;
-//    }
-
-//	public int add(String numbers) {
-//	    if (numbers.isEmpty()) {
-//	        return 0;
-//	    }
-//	    return Integer.parseInt(numbers);
-//	}
-
-	/**
-	 * Add Two Numbers
-	 */
-//	public int add(String numbers) {
-//	    if (numbers.isEmpty()) {
-//	        return 0;
-//	    }
-//	    //split string
-//	    String[] numArray = numbers.split(",");
-//	    int sum = 0;
-//	    for (String num : numArray) {
-//	        sum += Integer.parseInt(num);
-//	    }
-//	    return sum;
-//	}
-
-	/**
-	 * Test for Newline Delimiters
-	 */
-//	public int add(String numbers) {
-//	    if (numbers.isEmpty()) {
-//	        return 0;
-//	    }
-//	    //replace \n by ,
-//	    numbers = numbers.replace("\n", ",");
-//	    //split string
-//	    String[] numArray = numbers.split(",");
-//	    int sum = 0;
-//	    for (String num : numArray) {
-//	        sum += Integer.parseInt(num);
-//	    }
-//	    return sum;
-//	}
-
-	/**
-	 * Test for Custom Delimiters
-	 */
-//	public int add(String numbers) {
-//		if (numbers.isEmpty()) {
-//			return 0;
-//		}
-//		
-//		String delimiter = ",";
-//		if (numbers.startsWith("//")) {
-//			int delimiterIndex = numbers.indexOf("\n");
-//			delimiter = numbers.substring(2, delimiterIndex);
-//			numbers = numbers.substring(delimiterIndex + 1);
-//		}
-//		// replace \n by delimiter
-//		numbers = numbers.replace("\n", delimiter);
-//		// split string
-//		String[] numArray = numbers.split(delimiter);
-//		int sum = 0;
-//		for (String num : numArray) {
-//			sum += Integer.parseInt(num);
-//		}
-//		return sum;
-//	}
-
-	/**
-	 * Test for Negative Numbers
-	 */
-//	public int add(String numbers) {
-//		if (numbers.isEmpty()) {
-//			return 0;
-//		}
-//
-//		String delimiter = ",";
-//		if (numbers.startsWith("//")) {
-//			int delimiterIndex = numbers.indexOf("\n");
-//			delimiter = numbers.substring(2, delimiterIndex);
-//			numbers = numbers.substring(delimiterIndex + 1);
-//		}
-//		// replace \n by delimiter
-//		numbers = numbers.replace("\n", delimiter);
-//		// split string
-//		String[] numArray = numbers.split(delimiter);
-//		int sum = 0;
-//		
-//		//handling any negative number in the number string
-//		List<Integer> negativeNumbers = new ArrayList<>();
-//		for (String num : numArray) {
-//			int value = Integer.parseInt(num);
-//			if (value < 0) {
-//				negativeNumbers.add(value);
-//			} else {
-//				sum += value;
-//			}
-//		}
-//
-//		//if negative found throw error
-//		if (!negativeNumbers.isEmpty()) {
-//			throw new RuntimeException("Negative numbers not allowed: " + negativeNumbers);
-//		}
-//
-//		return sum;
-//	}
+	private static String formatExceptionMessage(List<Integer> negativeNumbers) {
+		//build exception message
+		StringBuilder message = new StringBuilder("Negative numbers not allowed: ");
+		for (int i = 0; i < negativeNumbers.size(); i++) {
+			message.append("[").append(negativeNumbers.get(i)).append("]");
+			if (i < negativeNumbers.size() - 1) {
+				message.append(",");
+			}
+		}
+		return message.toString();
+	}
 }
